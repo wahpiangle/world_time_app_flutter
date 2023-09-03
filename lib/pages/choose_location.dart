d75d29dc7c13e8880500c601a728acb049fc019e
quanming <qmtan17@gmail.com>
2023-09-03 23:10:46 +0800

    async code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,16 +8,35 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  Future getData() async {
+    String user = await Future.delayed(Duration(seconds: 2), () {
+      return 'user info';
+    });
+
+    String statement = await Future.delayed(Duration(seconds: 2), () {
+      return ('$user is fetched');
+    });
+
+    print(statement);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    print('test');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Choose a location'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: Text('choose'),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Choose a location'),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Text('d'));
   }
 }
